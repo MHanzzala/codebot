048e0afcda24a31c6ba763215a4fbadfa45d5e6e
hhh <hanzalakhokhar@gmail.com>
2023-02-23 03:55:23 +0500

    v1

--- codebot/website/views.py
+++ codebot/website/views.py
@@ -4,4 +4,15 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    lang_list = ['c', 'clike', 'cpp', 'csharp', 'css', 'dart', 'django', 'go', 'html', 'java', 'javascript', 'markup', 'markup-templating', 'matlab',
+                 'mongodb', 'objectivec', 'perl', 'php', 'powershell', 'python', 'r', 'regex', 'ruby', 'rust', 'sass', 'scala', 'sql', 'swift', 'yaml']
+
+    if request.method == 'POST':
+        code = request.POST['code']
+        lang = request.POST['lang']
+        return render(request, 'home.html',
+                      {
+                          'lang_list': lang_list,
+                          'code': code, 'lang': lang})
+
+    return render(request, 'home.html', {'lang_list': lang_list})
