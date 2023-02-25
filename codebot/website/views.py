f4383406885e7c6de5ce850437b4f40bb71f91ac
hhh <hanzalakhokhar@gmail.com>
2023-02-25 12:07:41 +0500

    v1

--- codebot/website/views.py
+++ codebot/website/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 import openai
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 
 # Create your views here.
 
@@ -81,3 +84,42 @@ def suggest(request):
                 return render(request, 'suggest.html', {'lang_list': lang_list, 'response': e, 'lang': lang})
 
     return render(request, 'suggest.html', {'lang_list': lang_list})
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been successfully logged in")
+            return redirect('home')
+        else:
+            messages.success(request, "Error Logging in. Please try again!")
+            return redirect('home')
+    else:
+        return render(request, "home.html", {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out successfully")
+    return redirect('home')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have been successfully registered")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+
+    return render(request, 'register.html', {'form': form})
