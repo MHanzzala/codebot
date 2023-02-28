2980860ca1caa991279007ec976becce6ee8619e
hhh <hanzalakhokhar@gmail.com>
2023-02-28 12:22:07 +0500

    v1

--- codebot/website/views.py
+++ codebot/website/views.py
@@ -109,17 +109,18 @@ def logout_user(request):
 
 
 def register_user(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            messages.success(request, "You have been successfully registered")
-            return redirect('home')
-    else:
-        form = SignUpForm()
+	if request.method == "POST":
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, "You have been registered successfully.")
+			return redirect('home')
 
-    return render(request, 'register.html', {'form': form})
+	else:
+		form = SignUpForm()
+
+	return render(request, 'register.html', {"form": form})
